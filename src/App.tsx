30ccd8e1522e5dd6243078656c93128031b67c25
samuel <samuel.slavik@protonmail.com>
2025-07-19 13:12:13 +0200

    Add color scheme and other css variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import { ExampleComponent } from './components/exampleComponent/ExampleComponent';
-import { Dummy } from '@musica-sacra/dummy';
 
 export function App() {
     return (
         <div className="App">
             <ExampleComponent title={'Cantus Catholici'} />
-            <Dummy dummyText={'Cantus Catholici dummy'} />
         </div>
     );
 }
