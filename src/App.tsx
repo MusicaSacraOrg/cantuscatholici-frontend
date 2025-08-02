f31d6abd405fe198408560e7e499475520b66c85
Zelo <lukyy6329@gmail.com>
2025-08-02 21:55:18 +0200

    Replace ExampleComponent with Navbar in App
    Swapped out the ExampleComponent for the Navbar component in the App, updating the main UI structure.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { ExampleComponent } from './components/exampleComponent/ExampleComponent';
 import { NotificationsContextProvider } from '@musica-sacra/notifications';
+import { Navbar } from './components/navbar/Navbar';
 
 export function App() {
     return (
         <div className="App">
             <NotificationsContextProvider>
-                <ExampleComponent title={'Cantus Catholici'} />
+                <Navbar />
             </NotificationsContextProvider>
         </div>
     );
