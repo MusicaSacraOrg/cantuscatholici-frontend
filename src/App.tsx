5bf76172db47eff782c0ec90b2e08b487796be2b
samuel <samuel.slavik@protonmail.com>
2025-08-11 19:18:51 +0200

    Add new plugin @musica-sacra/layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,19 @@ import { ExampleComponent } from './components/exampleComponent/ExampleComponent
 import { NotificationsContextProvider } from '@musica-sacra/notifications';
 import { Navbar } from './components/navbar/Navbar';
 import { Footer } from './components/footer/Footer';
+import { LayoutWithSidebar } from '@musica-sacra/layout';
 
 export function App() {
     return (
         <div className="App">
             <NotificationsContextProvider>
                 <Navbar />
-                <ExampleComponent title={'Cantus Catholici'} />
+                <LayoutWithSidebar
+                    isPageLayout={true}
+                    sidebar={<div>Here will be sidebar</div>}
+                >
+                    <ExampleComponent title={'Cantus Catholici'} />
+                </LayoutWithSidebar>
                 <Footer />
             </NotificationsContextProvider>
         </div>
